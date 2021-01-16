0616d723a44d9cd2c93596ffa40ccb59319f6e6a
TPGPL <mateuszkwiatkowski2002@gmail.com>
2021-01-16 15:56:04 +0100

    Add default game variables

---
+++ ping-pong/defaults.py
@@ -0,0 +1,11 @@
+from rule import Rule
+
+default_round_count = 3
+default_rules = [Rule("5", "ping", "DIVISION"), Rule("7", "pong", "DIVISION")]
+default_tour_time = 3
+default_difficulty = "M"
+default_tour_interval = 0.5
+default_life_count_easy = 5
+default_life_count_medium = 3
+default_life_count_hard = 1
+default_show_rules = True
